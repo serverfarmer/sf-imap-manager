bb38c6a7efe449c76410c2f799ec708f9d09154b
Tomasz Klim <github@tomaszklim.pl>
2017-10-20 12:42:24 +0200

    new sf-repos extension

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/farm/scripts/setup/role.sh sf-farm-manager
+/opt/farm/scripts/setup/extension.sh sf-farm-manager
 
 ln -sf /opt/farm/ext/imap-manager/add-imap-user.sh /usr/local/bin/add-imap-user
