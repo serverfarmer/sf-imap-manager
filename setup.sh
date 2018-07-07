12353c867c88a24427941c03c3692effc854ae96
Tomasz Klim <github@tomaszklim.pl>
2018-07-07 15:05:57 +0200

    use sf-net-utils extension

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+/opt/farm/scripts/setup/extension.sh sf-net-utils
 /opt/farm/scripts/setup/extension.sh sf-farm-manager
 
 ln -sf /opt/farm/ext/imap-manager/add-imap-user.sh /usr/local/bin/add-imap-user
