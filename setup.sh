6b25ec871a79aec362eac6f61c87b90f6c36d6c2
Tomasz Klim <github@tomaszklim.pl>
2020-01-28 23:23:00 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@
 /opt/farm/scripts/setup/extension.sh sf-net-utils
 /opt/farm/scripts/setup/extension.sh sf-passwd-utils
 /opt/farm/scripts/setup/extension.sh sf-farm-manager
-
-ln -sf /opt/farm/ext/imap-manager/add-imap-user.sh /usr/local/bin/add-imap-user
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/add-imap-user ]; then
-	rm -f /usr/local/bin/add-imap-user
-fi
